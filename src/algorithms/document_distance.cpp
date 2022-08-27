26e7d6c0bc3fbcafb96534abaa4739472150a641
Fahmi Ghediri <fahmi.gh@gmail.com>
2022-08-27 23:05:04 +0200

    Implement get_word_count function

--- src/algorithms/document_distance.cpp
+++ src/algorithms/document_distance.cpp
@@ -8,7 +8,8 @@
 
 class DocumentDistance {
  public:
-  std::vector<std::string> extract_words_from_document(const std::string& doc) {
+  auto extract_words_from_document(const std::string& doc)
+      -> std::vector<std::string> {
     std::vector<std::string> words = {};
     std::string buffer = {};
     for (auto i = doc.begin(); i != doc.end(); ++i) {
@@ -22,7 +23,18 @@ class DocumentDistance {
     return words;
   }
 
+  auto get_word_count(const std::vector<std::string>& words)
+      -> std::unordered_map<std::string, size_t> {
+    auto word_count = std::unordered_map<std::string, size_t>{};
+    for (const auto& elem : words) {
+      word_count[elem]++;
+    }
+    return word_count;
+  }
+
   double document_distance(const std::string& doc1, const std::string& doc2) {
+    auto words_doc1 = extract_words_from_document(doc1);
+    auto words_doc2 = extract_words_from_document(doc2);
     return 0;
   }
 };
@@ -36,4 +48,13 @@ TEST(document_distance_test, extract_words_from_document_success) {
   EXPECT_EQ(result.size(), 18);
   EXPECT_EQ("This", result[0]);
   EXPECT_EQ("stop", result[17]);
+}
+
+TEST(document_distance_test, get_word_count_success) {
+  std::vector<std::string> words = {"foo", "bar", "baz", "foo", "foo", "baz"};
+  DocumentDistance parser;
+  auto result = parser.get_word_count(words);
+  EXPECT_EQ(3, result["foo"]);
+  EXPECT_EQ(2, result["baz"]);
+  EXPECT_EQ(1, result["bar"]);
 }
