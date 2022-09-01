b84c3dc350c3e4c81a6324d9052450e27de21a36
Fahmi Ghediri <fahmi.gh@gmail.com>
2022-09-01 19:42:27 +0200

    Add insertion sort

---
+++ src/algorithms/sorting/insertion_sort.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+class InsertionSort {
+ public:
+  void sort(std::vector<int>& v) {
+    for (auto i = 1; i < v.size(); ++i) {
+      auto key = v[i];
+      auto j = i - 1;
+      while (j >= 0 and v[j] > key) {
+        v[j + 1] = v[j];
+        --j;
+      }
+      v[j + 1] = key;
+    }
+  }
+};
+
+TEST(insertion_sort_test, sort_empty_array) {
+  std::vector<int> v{};
+  InsertionSort sorter;
+  sorter.sort(v);
+  EXPECT_EQ(v, std::vector<int>{});
+}
+
+TEST(insertion_sort_test, sort_one_element_vector) {
+  std::vector<int> v{42};
+  InsertionSort sorter;
+  sorter.sort(v);
+  EXPECT_EQ(v, std::vector<int>{42});
+}
+
+TEST(insertion_sort_test, sort_two_element_vector) {
+  std::vector<int> v{42, 31};
+  InsertionSort sorter;
+  sorter.sort(v);
+  EXPECT_EQ(v, std::vector<int>({31, 42}));
+}
+
+TEST(insertion_sort_test, sort_reverse_sorted_elements) {
+  std::vector<int> v{90, 80, 70, 70, 60, 50, 40, 10};
+  InsertionSort sorter;
+  sorter.sort(v);
+  EXPECT_EQ(v, std::vector<int>({10, 40, 50, 60, 70, 70, 80, 90}));
+}
+
+TEST(insertion_sort_test, sort_already_sorted_elements) {
+  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  InsertionSort sorter;
+  sorter.sort(v);
+  EXPECT_EQ(v, std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}));
+}
+
+TEST(insertion_sort_test, sort_random_array) {
+  std::vector<int> v{359,     478,       23548, 13456, 654,
+                     1236548, 215487965, 12,    35,    89};
+  InsertionSort sorter;
+  sorter.sort(v);
+  EXPECT_EQ(v, std::vector<int>({12, 35, 89, 359, 478, 654, 13456, 23548,
+                                 1236548, 215487965}));
+}
